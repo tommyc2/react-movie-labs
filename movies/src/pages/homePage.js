901c4cb398b9598465db17e145aa878abe60804c
tommyc2 <20101841@mail.wit.ie>
2024-10-08 12:22:39 +0100

    Added filtering of movie list - Movie lab 1 complete!

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -5,7 +5,29 @@ import Header from '../components/headerMovieList';
 import FilterCard from "../components/filterMoviesCard";
 
 const HomePage = () => {
+
     const [movies, setMovies] = useState([]);
+    const [nameFilter, setNameFilter] = useState(""); // filter
+    const [genreFilter, setGenreFilter] = useState("0"); // for list of genres (from APi)
+
+    const genreId = Number(genreFilter);
+
+    let displayedMovies = movies
+        .filter((m) => {
+            return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+        })
+        .filter((m) => {
+            return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+        });
+
+    const handleChange = (type, value) => {
+        if (type === "name") {
+            setNameFilter(value);
+        }
+        else {
+            setGenreFilter(value);
+        }
+    };
 
     useEffect(() => {
         fetch(
@@ -28,9 +50,13 @@ const HomePage = () => {
             </Grid>
             <Grid container sx={{flex: "500px 1 0"}}>
                 <Grid key="find" size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-                    <FilterCard />
+                    <FilterCard
+                        onUserInput={handleChange}
+                        titleFilter={nameFilter}
+                        genreFilter={genreFilter}
+                    />
                 </Grid>
-                <MovieList movies={movies}></MovieList>
+                <MovieList movies={displayedMovies} />
             </Grid>
         </Grid>
     );
