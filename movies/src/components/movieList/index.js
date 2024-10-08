c9e054e76bc5f3518ffaead20e7e6af8cea685af
tommyc2 <20101841@mail.wit.ie>
2024-10-08 11:35:26 +0100

    List of movies displayed.

---
+++ movies/src/components/movieList/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Movie from "../movieCard/";
+import Grid from "@mui/material/Grid2";
+
+const MovieList = (props) => {
+    let movieCards = props.movies.map((m) => (
+        <Grid key={m.id} size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
+            <Movie key={m.id} movie={m} />
+        </Grid>
+    ));
+    return movieCards;
+};
+
+export default MovieList;
--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,6 +1,6 @@
 import React from "react";
+import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid2";
-import MovieCard from '../components/movieCard'
 
 const HomePage = (props) => {
     const movies = props.movies;
@@ -10,8 +10,8 @@ const HomePage = (props) => {
             <Grid size={12}>
                 <h1> HomePage </h1>
             </Grid>
-            <Grid size={3}>
-                <MovieCard movie={movies[0]} />
+            <Grid container>
+                <MovieList movies={movies}></MovieList>
             </Grid>
         </Grid>
     );
