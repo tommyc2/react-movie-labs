504eabb7940f50814810529fc461aab8be2ed22f
tommyc2 <20101841@mail.wit.ie>
2024-10-22 12:15:49 +0100

    Refactor: Favourite movie list now stored in a context.

--- movies/src/index.js
+++ movies/src/index.js
@@ -8,6 +8,7 @@ import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
+import MoviesContextProvider from "./contexts/moviesContext";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -24,6 +25,7 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
         <BrowserRouter>
             <SiteHeader />
+            <MoviesContextProvider>
             <Routes>
                 <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
                 <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
@@ -31,6 +33,7 @@ const App = () => {
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={ <Navigate to="/" /> } />
             </Routes>
+            </MoviesContextProvider>
         </BrowserRouter>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
