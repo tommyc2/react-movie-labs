948aadbd79b49f0c900281c50e7e8ccfd4656333
tommyc2 <20101841@mail.wit.ie>
2024-10-22 13:14:15 +0100

    Added Upcoming movies page

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,6 +10,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+import UpcomingMoviesPage from "./pages/upcomingMovies";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -32,6 +33,7 @@ const App = () => {
                 <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
                 <Route path="/movies/:id" element={<MoviePage />} />
                 <Route path="/" element={<HomePage />} />
+                <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
                 <Route path="*" element={ <Navigate to="/" /> } />
                 <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
             </Routes>
---
+++ movies/src/pages/upcomingMovies.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {getMovies, getUpcomingMovies} from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+
+const UpcomingMoviesPage = (props) => {
+
+    const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
+
+    if (isLoading) { return <Spinner /> }
+    if (isError) { return <h1>{error.message}</h1> }
+
+    const movies = data.results;
+
+    // Redundant, but necessary to avoid app crashing.
+    const favorites = movies.filter(m => m.favorite)
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+    const addToFavorites = (movieId) => true
+
+    return (
+        <PageTemplate
+            title="Upcoming Movies"
+            movies={movies}
+            action={(movie) => {
+                return <AddToFavoritesIcon movie={movie} />
+            }}
+        />
+    );
+};
+export default UpcomingMoviesPage;
+
